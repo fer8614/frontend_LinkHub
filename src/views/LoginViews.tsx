b8fc603aa977b7c22775a5110b109e085bdd3905
Fernando <fernando.inversionmf@gmail.com>
2025-03-08 12:39:54 -0800

    Create routes Login and Regiter

--- src/views/LoginViews.tsx
+++ src/views/LoginViews.tsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function LoginView() {
-  return <div className="text-6xl font-bold">LoginView</div>;
+  return (
+    <>
+      <div className="text-6xl font-bold">LoginView</div>;
+      <nav>
+        <Link to="/auth/register">Don't have an account? Create one here</Link>
+      </nav>
+    </>
+  );
 }
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function RegisterView() {
-  return <div className=" text-6xl font-bold">RegisterView</div>;
+  return (
+    <>
+      <div className="text-6xl font-bold">RegisterView</div>;
+      <nav>
+        <Link to="/auth/login">Already have an account? Sign in</Link>
+      </nav>
+    </>
+  );
 }
