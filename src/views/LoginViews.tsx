e8c697235ef25c3f2d9a1e2c511fdc6f6c72a110
Fernando <fernando.inversionmf@gmail.com>
2025-03-08 20:51:48 -0800

    Create Layout and logo

--- src/views/LoginViews.tsx
+++ src/views/LoginViews.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <div className="text-6xl font-bold">LoginView</div>;
       <nav>
         <Link to="/auth/register">Don't have an account? Create one here</Link>
       </nav>
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export default function RegisterView() {
   return (
     <>
-      <div className="text-6xl font-bold">RegisterView</div>;
       <nav>
         <Link to="/auth/login">Already have an account? Sign in</Link>
       </nav>
