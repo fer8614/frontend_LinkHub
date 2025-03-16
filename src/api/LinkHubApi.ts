229cb3747c74f73c65bd940d1bde30baae7dd283
Fernando <fernando.inversionmf@gmail.com>
2025-03-15 17:28:17 -0700

    Sending the request with the JWT in React Query

--- src/api/LinkHubApi.ts
+++ src/api/LinkHubApi.ts
@@ -2,9 +2,14 @@ import { isAxiosError } from "axios";
 import api from "../config/axios";
 
 export async function getUser() {
+  const token = localStorage.getItem("AUTH_TOKEN_LH");
   try {
-    const { data } = await api(`/user`);
-    console.log(data);
+    const { data } = await api(`/user`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
