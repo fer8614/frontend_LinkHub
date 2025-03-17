cb15970d2c59bb7a64aa83b6f065ad1a93b267ce
Fernando <fernando.inversionmf@gmail.com>
2025-03-16 20:05:18 -0700

    Update Profile.tsx

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -1,6 +1,24 @@
+import { useForm } from "react-hook-form";
+import ErrorMessage from "../components/errorMessage";
+
 export default function ProfileView() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { handle: "", description: "" },
+  });
+
+  const handleUserProfileForm = (formData) => {
+    console.log(formData);
+  };
+
   return (
-    <form className="bg-white p-10 rounded-lg space-y-5" onSubmit={() => {}}>
+    <form
+      className="bg-white p-10 rounded-lg space-y-5"
+      onSubmit={handleSubmit(handleUserProfileForm)}
+    >
       <legend className="text-2xl text-slate-800 text-center">
         Edit Information
       </legend>
@@ -10,7 +28,11 @@ export default function ProfileView() {
           type="text"
           className="border-none bg-slate-100 rounded-lg p-2"
           placeholder="handle or Username"
+          {...register("handle", {
+            required: "Handle or Username is required",
+          })}
         />
+        {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
@@ -18,7 +40,13 @@ export default function ProfileView() {
         <textarea
           className="border-none bg-slate-100 rounded-lg p-2"
           placeholder="Your Description"
+          {...register("description", {
+            required: "Description is required",
+          })}
         />
+        {errors.description && (
+          <ErrorMessage>{errors.description.message}</ErrorMessage>
+        )}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
