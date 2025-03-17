738b23f7ea5473d4c6c91a3d27fb351bdcabb930
Fernando <fernando.inversionmf@gmail.com>
2025-03-16 19:22:50 -0700

    Getting user information and displaying it in the component

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,8 @@
-import { Link, Outlet } from "react-router-dom";
-import { Toaster } from "sonner";
 import { useQuery } from "@tanstack/react-query";
 import { Navigate } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
-import NavigationTabs from "../components/NavigationTabs";
 import { getUser } from "../api/LinkHubApi";
+import LinkHub from "../components/LinkHub";
 
 export default function AppLayout() {
   const { data, isLoading, isError } = useQuery({
@@ -24,47 +22,5 @@ export default function AppLayout() {
     return <Navigate to="/auth/login" />;
   }
 
-  return (
-    <>
-      <header className="bg-slate-800 py-5">
-        <div className="mx-auto max-w-5xl flex flex-col md:flex-row items-center md:justify-between">
-          <div className="w-full p-5 lg:p-0 md:w-1/3">
-            <img src="/logo.png" className="w-full block" />
-          </div>
-          <div className="md:w-1/3 md:flex md:justify-end">
-            <button
-              className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
-              onClick={() => {}}
-            >
-              Log Out
-            </button>
-          </div>
-        </div>
-      </header>
-      <div className="bg-gray-100  min-h-screen py-10">
-        <main className="mx-auto max-w-5xl p-10 md:p-0">
-          <NavigationTabs />
-
-          <div className="flex justify-end">
-            <Link
-              className="font-bold text-right text-slate-800 text-2xl"
-              to={""}
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              Visit My Profile
-            </Link>
-          </div>
-
-          <div className="flex flex-col md:flex-row gap-10 mt-10">
-            <div className="flex-1 ">
-              <Outlet />
-            </div>
-            <div className="w-full md:w-96 bg-slate-800 px-5 py-10 space-y-6"></div>
-          </div>
-        </main>
-      </div>
-      <Toaster position="top-right" />
-    </>
-  );
+  if (data) return <LinkHub data={data} />;
 }
