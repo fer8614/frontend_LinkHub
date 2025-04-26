6335c2d6b043a25bcfd2eb57deb12d518d9bc706
Fernando <fernando.inversionmf@gmail.com>
2025-04-26 16:04:47 -0700

    Querying the Hub with React Query

--- src/views/HandleView.tsx
+++ src/views/HandleView.tsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUserByHandle } from "../api/LinkHubApi";
 
 export default function HandleView() {
+  const params = useParams();
+  const handle = params.handle!;
+  const { data, isLoading, error } = useQuery({
+    queryFn: () => getUserByHandle(handle),
+    queryKey: ["handle", handle],
+    retry: 1,
+  });
+  console.log(isLoading);
+  console.log(error);
+  console.log(data);
   return (
     <div>
       <h1>HandleView</h1>
