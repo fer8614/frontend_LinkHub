d2abf2ea7253ac087dd0c23fd4d03402e3a3e0aa
Fernando <fernando.inversionmf@gmail.com>
2025-04-26 23:43:21 -0700

    Getting started with the homepage and adding a background image

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -4,6 +4,20 @@ export default function HomeView() {
     return (
         <div>
             <Header />
+
+            <main className="bg-gray-100 min-h-screen py-10 lg:bg-[url('/bg.svg')] bg-no-repeat bg-right-top lg:bg-[size:50%]">
+                <div className="mx-auto max-w-5xl mt-10">
+                    <div className="lg:w-1/2 px-10 lg:px-0 space-y-6">
+                        <h1 className="text-6xl font-black">
+                            All your <span className="text-cyan-400">Social Networks</span> in one link
+                        </h1>
+
+                        <p className="text-xl text-slate-800">
+                        Join our developer community by sharing your social media accounts, including your GitHub, LinkedIn, Instagram, YouTube, Facebook, and more profiles.
+                        </p>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
