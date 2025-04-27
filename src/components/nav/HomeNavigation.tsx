63f498c3bc70a8ba9b68a8c9661b9d8d393a0da8
Fernando <fernando.inversionmf@gmail.com>
2025-04-26 19:08:30 -0700

    Creating a Reusable Component for the Logo and Navigations

--- src/components/nav/HomeNavigation.tsx
+++ src/components/nav/HomeNavigation.tsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 export default function HomeNavigation() {
     return (
         <>
-            <Link to="/auth/login" className="text-white p-2 uppercase font-black text-xs cursor-pointer">Login</Link>
+            <Link to="/auth/login" className="text-white p-2 uppercase font-black text-xs cursor-pointer">
+                Login
+            </Link>
+            <Link to="/auth/register" className="bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer">
+                Register
+            </Link>
         </>
     );
 }
