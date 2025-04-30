713d134bcacf28c2a069ae74c81e2356f72f1ea7
Fernando <fernando.inversionmf@gmail.com>
2025-04-29 19:11:33 -0700

    Logging out the user, removing the QueryKey and token from localStorage

--- src/components/nav/AdminNavigation.tsx
+++ src/components/nav/AdminNavigation.tsx
@@ -1,9 +1,20 @@
+import { useQueryClient } from "@tanstack/react-query";
+
 export default function AdminNavigation() {
-    return (
-        <button
-              className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
-              onClick={() => {}}
-            >
+
+  const queryClient = useQueryClient();
+  const logOut = () => {
+    localStorage.removeItem("AUTH_TOKEN_LH");
+    queryClient.invalidateQueries({
+      queryKey: ["user"],
+    });
+  };
+
+  return (
+    <button
+      className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
+      onClick={logOut}
+    >
               Log Out
         </button>
     );
